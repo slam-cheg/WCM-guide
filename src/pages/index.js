8a7fda0597d328ac4e22373c26e9fc3e0505e711
Lev Shushkov <levashushkov@yandex.ru>
2022-01-18 16:59:06 +0300

    home-screen + nav

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,23 @@
 import "./index.css";
+import logo from "../images/logo.png";
+import homescreen from "../images/homescreen.jpg";
+
+const menuButtons = document.querySelectorAll(".menu__item");
+
+menuButtons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        changeButtonState(btn);
+    });
+});
+
+function changeButtonState(btn) {
+    const activeClass = "menu__item_active";
+    if (!btn.classList.contains(activeClass)) {
+        menuButtons.forEach((btn) => {
+            btn.classList.remove(activeClass);
+        });
+        btn.classList.add(activeClass);
+    } else {
+        btn.classList.remove(activeClass);
+    }
+}
